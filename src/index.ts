35337977a67c63e8f728ea1def181be12b2029db
Nik Omel <nikomelf@gmail.com>
2021-05-18 18:43:56 -0700

    chrome: testing github actions

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import app from "./app";
 const PORT = process.env.PORT || 3000;
 
 (async () => {
+  console.log("TEST");
+
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
 
